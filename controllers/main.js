c4639e908a951d47ca5549d4237a7211a42ea118
warlyware <danward@gmail.com>
2015-06-29 09:35:00 -0700

    Fix index.html for gh-pages

--- controllers/main.js
+++ controllers/main.js
@@ -29,15 +29,15 @@ twatter.controller('MainCtrl', function($scope, $rootScope, $firebaseObject, $fi
   }
 
   $scope.followUser = function(index) {
-    console.log(index);
-    var length = $scope.tweets.length - 1; // Get array length
-    var idx = length - index; // Get proper index by subtracting $index from length
-    var userToFollow = usersRef.child(tweetsArray[idx]);
-    var userToFollowObj = $firebaseObject(userToFollow);
-    console.log(userToFollowObj);
-    userToFollow.update({
-      follow: $scope.tweets[idx].userID
-    });
+    // console.log(index);
+    // var length = $scope.tweets.length - 1; // Get array length
+    // var idx = length - index; // Get proper index by subtracting $index from length
+    // var userToFollow = usersRef.child(tweetsArray[idx]);
+    // var userToFollowObj = $firebaseObject(userToFollow);
+    // console.log(userToFollowObj);
+    // userToFollow.update({
+    //   follow: $scope.tweets[idx].userID
+    // });
     // console.log(usersObj[$scope.tweets[idx].userID].following);
   }
 
